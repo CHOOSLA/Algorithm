cd5303f0ec5e4878171030c6de2bcb9379496dfe
Choo ChangWoo <cnckddn0146@gmail.com>
2024-08-01 17:31:21 +0900

    귀납적으로 풂

--- BaekJoon/1074/play.cpp
+++ BaekJoon/1074/play.cpp
@@ -1,10 +1,35 @@
-#include "iostream"
+#include <iostream>
 
 using namespace std;
-
-int main(int argc, char const *argv[])
+int func(int n, int r, int c)
 {
-    int n = 1;
-    cout << (n << 1) << endl;
+    if (n == 0)
+        return 0;
+
+    // 비트 연산자 << 은 1승을 증가시킨다.
+    int half = 1 << (n - 1);
+    if (r < half && c < half)
+    {
+        return func(n - 1, r, c);
+    }
+    if (r < half && c >= half)
+    {
+        return half * half + func(n - 1, r, c - half);
+    }
+    if (r >= half && c < half)
+    {
+        return 2 * half * half + func(n - 1, r - half, c);
+    }
+
+    return 3 * half * half + func(n - 1, r - half, c - half);
+}
+
+int main(void)
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int n, r, c;
+    cin >> n >> r >> c;
+    cout << func(n, r, c);
     return 0;
 }
