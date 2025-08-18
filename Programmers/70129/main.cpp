90138ba87b9a258c232df06d31229d2ae3ab2d3d
Choosla <cnckddn0146@gmail.com>
2025-08-18 20:45:11 +0900

    [LV2] : 이진 변환 반복하기 - 프로그래머스

---
+++ Programmers/70129/main.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string numToDigi(int num) {
+  string result = "";
+
+  while (true) {
+    if (num == 1 || num == 0) {
+      result += num + 48;
+      break;
+    }
+
+    // 비트 연산자 시행
+    int tmp = num & 1;
+    cout << tmp << endl;
+
+    // 이것을 글자를 붙임
+    result += tmp + 48;
+    // 오른쪽으로 쉬프트 연산
+    num = num >> 1;
+  }
+
+  string out = "";
+
+  for (int i = result.length() - 1; i >= 0; i--) {
+    out += result.at(i);
+  }
+
+  return out;
+}
+
+int rmCount = 0;
+string removeZero(string str) {
+  string result = "";
+  for (char a : str) {
+    if (a == '1') {
+      result += a;
+    } else {
+      rmCount++;
+    }
+  }
+  return result;
+}
+
+vector<int> solution(string s) {
+  vector<int> answer;
+  int count = 0;
+  while (s != "1") {
+    count++;
+    s = removeZero(s);
+    s = numToDigi(s.length());
+  }
+
+  answer.push_back(count);
+  answer.push_back(rmCount);
+  return answer;
+}
