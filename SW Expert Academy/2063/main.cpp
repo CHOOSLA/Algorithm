e591fa5ac3be5cd68fd14b6e3796afd4760b60a0
CHOOSLA <ccw.oopsla@gmail.com>
2026-04-30 17:42:49 +0900

    SEA(D1) : 2063. 중간값 찾기 - Sorting

---
+++ SW Expert Academy/2063/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+int main() {
+    int N; cin >> N;
+    vector<int> A(N);
+    for (int i = 0; i < N; ++i) cin >> A[i];
+    sort(A.begin(), A.end());
+    cout << A[N / 2] << "\n";
+    return 0;
+}
