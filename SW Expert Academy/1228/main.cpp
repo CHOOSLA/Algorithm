ec19794f4cbc22fe59ced5c3f133c61572a3263a
CHOOSLA <ccw.oopsla@gmail.com>
2026-05-08 16:29:58 +0900

    SEA(D3) : 1228. 암호문1 - LinkedList

---
+++ SW Expert Academy/1228/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int main() {
+    for (int t = 1; t <= 10; ++t) {
+        int N; cin >> N;
+        vector<int> code(N);
+        for (int i = 0; i < N; ++i) cin >> code[i];
+        int cmd_cnt; cin >> cmd_cnt;
+        for (int c = 0; c < cmd_cnt; ++c) {
+            char cmd; cin >> cmd;
+            int x, y; cin >> x >> y;
+            vector<int> temp(y);
+            for (int i = 0; i < y; ++i) cin >> temp[i];
+            code.insert(code.begin() + x, temp.begin(), temp.end());
+        }
+        cout << "#" << t << " ";
+        for (int i = 0; i < 10; ++i) cout << code[i] << " ";
+        cout << "\n";
+    }
+    return 0;
+}
