35e76e7bbf580ae1b0ca31069173f069f6f1b959
choosla <ccw.oopsla@gmail.com>
2026-05-17 21:30:22 +0900

    SEA(D3) : 1217. 거듭 제곱 - 재귀

---
+++ SW Expert Academy/1217/main.cpp
@@ -0,0 +1,20 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+
+using namespace std;
+
+int power(int n, int m) {
+    if (m == 0) return 1;
+    return n * power(n, m - 1);
+}
+
+int main() {
+    int tc;
+    for (int t = 0; t < 10; ++t) {
+        cin >> tc;
+        int n, m;
+        cin >> n >> m;
+        cout << "#" << tc << " " << power(n, m) << endl;
+    }
+    return 0;
+}
