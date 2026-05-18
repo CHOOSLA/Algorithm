d426aeb02e0e05366ea7077529fbca351ee18aae
ChooChangwoo <ccw.oopsla@gmail.com>
2026-05-18 17:06:48 +0900

    [Wrong Answer] DateTime to DateTime (8ms, 0MB)

---
+++ trail2/DateTime to DateTime/datetime-to-datetime.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+using namespace std;
+
+int a, b, c;
+
+int main() {
+    cin >> a >> b >> c;
+
+    // Please write your code here.
+    int mon = 11, day = 11, min = 11;
+    int result = 0;
+
+    while(true){
+        if(mon == a && day == b && min == c){
+            break;
+        }
+
+        result++;
+        min++;
+
+        if(min == 60){
+            day++;
+            min = 0;
+        }
+
+        if(day == 24){
+            mon++;
+            day = 0;
+        }
+    }
+
+    cout << (result ? result : -1) << endl;
+    return 0;
+}
