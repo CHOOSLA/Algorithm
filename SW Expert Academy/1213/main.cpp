27c676e437e9cf436f2cdde3d8786eaab8a51db7
choosla <ccw.oopsla@gmail.com>
2026-05-20 22:45:15 +0900

    SEA(D3) : 1213. String - 문자열 찾기

---
+++ SW Expert Academy/1213/main.cpp
@@ -0,0 +1,24 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    int tc;
+    for (int t = 0; t < 10; ++t) {
+        cin >> tc;
+        string target, source;
+        cin >> target >> source;
+
+        int count = 0;
+        size_t pos = source.find(target, 0);
+        while (pos != string::npos) {
+            count++;
+            pos = source.find(target, pos + 1);
+        }
+
+        cout << "#" << tc << " " << count << endl;
+    }
+    return 0;
+}
