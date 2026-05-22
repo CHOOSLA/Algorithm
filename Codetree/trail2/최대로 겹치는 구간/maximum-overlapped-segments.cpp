048df44887fb3e5ec0e3213c9243bd99b3357089
ChooChangwoo <ccw.oopsla@gmail.com>
2026-05-23 01:41:54 +0900

    [Wrong Answer] 최대로 겹치는 구간 (2ms, 0MB)

---
+++ Codetree/trail2/최대로 겹치는 구간/maximum-overlapped-segments.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int n;
+int x1[100], x2[100];
+
+int main() {
+    cin >> n;
+
+    for (int i = 0; i < n; i++) {
+        cin >> x1[i] >> x2[i];
+    }
+
+    // Please write your code here.
+    int check[201] ={};
+
+    for(int i=0; i < n; ++i){
+        for(int j=x1[i] + 100; j <= x2[i] + 100; ++j){
+            check[j]++;
+        }
+    }
+
+    int result_idx = 0;
+    int result = -1;
+    for(int i=0; i < 201; ++i){
+        result = max(result, check[i]);
+        result_idx = i;
+    }
+
+    cout << result;
+
+    return 0;
+}
