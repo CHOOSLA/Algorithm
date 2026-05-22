d139e6e1b249e98f2735600abb58d2e4ebb5f6a4
ChooChangwoo <ccw.oopsla@gmail.com>
2026-05-23 02:24:59 +0900

    [Wrong Answer] 왔다 갔던 구역 2 (2ms, 0MB)

---
+++ Codetree/trail2/왔다 갔던 구역 2/area-been-to-and-from2.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+using namespace std;
+
+int n;
+int x[100];
+char dir[100];
+
+int main() {
+    cin >> n;
+
+    for (int i = 0; i < n; i++) {
+        cin >> x[i] >> dir[i];
+    }
+
+    // Please write your code here.
+    int check[500] = {0,};
+    int now = 249;
+    for(int i=0; i < n; ++i){
+        int d = dir[i] == 'R' ? 1 : -1;
+        for(int j = 0; j < x[i] ; ++j,now += d){
+            check[now]++;
+        }
+    }
+
+    int result =0;
+    for(int i=0; i < 500; ++i){
+        if(check[i] >= 2){
+            result++;
+        }
+    }
+
+    cout << result;
+    return 0;
+}
