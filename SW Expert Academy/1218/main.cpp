c5296e20a747b8db67cae485c9e33f041262126a
choosla <ccw.oopsla@gmail.com>
2026-05-26 22:30:15 +0900

    SEA(D4) : 1218. 괄호 짝짓기 - Stack

---
+++ SW Expert Academy/1218/main.cpp
@@ -0,0 +1,37 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <stack>
+#include <string>
+
+using namespace std;
+
+int main() {
+    for (int tc = 1; tc <= 10; ++tc) {
+        int len;
+        cin >> len;
+        string s;
+        cin >> s;
+
+        stack<char> st;
+        bool possible = true;
+        for (char c : s) {
+            if (c == '(' || c == '[' || c == '{' || c == '<') {
+                st.push(c);
+            } else {
+                if (st.empty()) { possible = false; break; }
+                char top = st.top();
+                if ((c == ')' && top == '(') || (c == ']' && top == '[') || 
+                    (c == '}' && top == '{') || (c == '>' && top == '<')) {
+                    st.pop();
+                } else {
+                    possible = false;
+                    break;
+                }
+            }
+        }
+        if (!st.empty()) possible = false;
+
+        cout << "#" << tc << " " << (possible ? 1 : 0) << endl;
+    }
+    return 0;
+}
