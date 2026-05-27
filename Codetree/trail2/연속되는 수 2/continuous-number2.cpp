f6b798051dfb8c5d3523d6e8415ac949ae6e1b7c
ChooChangwoo <ccw.oopsla@gmail.com>
2026-05-27 16:24:02 +0900

    [Wrong Answer] 연속되는 수 2 (2ms, 0MB)

---
+++ Codetree/trail2/연속되는 수 2/continuous-number2.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int N;
+int arr[1000];
+
+int main() {
+    cin >> N;
+    for (int i = 0; i < N; i++) {
+        cin >> arr[i];
+    }
+
+    // Please write your code here.
+    for(int i=0; i < N; ++i){
+        
+    }
+
+    return 0;
+}
