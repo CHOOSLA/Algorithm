0c825e740fec1e0e150b365a319ca92d9a85255a
choosla <ccw.oopsla@gmail.com>
2026-05-28 21:40:22 +0900

    SEA(D4) : 1224. 계산기3 - Stack

---
+++ SW Expert Academy/1224/main.cpp
@@ -0,0 +1,63 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <string>
+#include <stack>
+#include <vector>
+
+using namespace std;
+
+int getPriority(char c) {
+    if (c == '+') return 1;
+    if (c == '*') return 2;
+    return 0;
+}
+
+int main() {
+    for (int tc = 1; tc <= 10; ++tc) {
+        int len;
+        cin >> len;
+        string s;
+        cin >> s;
+
+        string postfix = "";
+        stack<char> ops;
+        for (char c : s) {
+            if (isdigit(c)) {
+                postfix += c;
+            } else if (c == '(') {
+                ops.push(c);
+            } else if (c == ')') {
+                while (!ops.empty() && ops.top() != '(') {
+                    postfix += ops.top();
+                    ops.pop();
+                }
+                ops.pop();
+            } else {
+                while (!ops.empty() && ops.top() != '(' && getPriority(ops.top()) >= getPriority(c)) {
+                    postfix += ops.top();
+                    ops.pop();
+                }
+                ops.push(c);
+            }
+        }
+        while (!ops.empty()) {
+            postfix += ops.top();
+            ops.pop();
+        }
+
+        stack<int> res;
+        for (char c : postfix) {
+            if (isdigit(c)) {
+                res.push(c - '0');
+            } else {
+                int v2 = res.top(); res.pop();
+                int v1 = res.top(); res.pop();
+                if (c == '+') res.push(v1 + v2);
+                else res.push(v1 * v2);
+            }
+        }
+
+        cout << "#" << tc << " " << res.top() << endl;
+    }
+    return 0;
+}
