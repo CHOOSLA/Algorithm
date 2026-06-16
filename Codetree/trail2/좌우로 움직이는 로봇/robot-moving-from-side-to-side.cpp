48c2914fce5a1f4be30e1f54a34b243572cd55e4
ChooChangwoo <ccw.oopsla@gmail.com>
2026-06-16 21:00:35 +0900

    [Wrong Answer] 좌우로 움직이는 로봇 (2ms, 0MB)

---
+++ Codetree/trail2/좌우로 움직이는 로봇/robot-moving-from-side-to-side.cpp
@@ -0,0 +1,79 @@
+#include <iostream>
+
+using namespace std;
+
+int n, m;
+int t[50000];
+char d[50000];
+int t_b[50000];
+char d_b[50000];
+
+// 내가 작성한 코드
+int pos1[2000005];
+int pos2[2000005];
+
+int main() {
+    cin >> n >> m;
+
+    for (int i = 0; i < n; i++) {
+        cin >> t[i] >> d[i];
+    }
+
+    for (int i = 0; i < m; i++) {
+        cin >> t_b[i] >> d_b[i];
+    }
+
+    // Please write your code here.
+    // 바로 직전에는 다른 위치에 있다가 그 다음 번에 같은 위치에 오는 경우
+    // 만약에 바로 직전에도 같았다면 횟수 추가 x idx-1로 저장을 해야함
+    // 한 로봇이 멈춰도 다른 로봇이 멈추지 않을 경우가 있음
+    int time_a = 1;
+    for(int i=0; i < n; ++i){
+        for(int time=0; time < t[i]; ++time){
+            pos1[time_a] = pos1[time_a-1] + (d[i] == 'L' ? -1 : 1);
+            time_a++;
+        }
+    }
+
+    int time_b = 1;
+    for(int i=0; i < m; ++i){
+        for(int time=0; time < t_b[i]; ++time){
+            pos2[time_b] = pos2[time_b-1] + (d_b[i] == 'L' ? -1 : 1);
+            time_b++;
+        }
+    }
+
+    int result = 0;
+
+    // 각 로봇이 움직인 위치는 기록이됨
+    int max_time = max(time_a, time_b);
+    // 두 로봇이 모두 멈출 때 까지 움직임
+    int now_time = 1;
+    while(now_time < max_time){
+        int now_a = now_time;
+        int now_b = now_time;
+
+        if(now_time >= time_a){
+            now_a = time_a - 1;
+        }
+
+        if(now_time >= time_b){
+            now_b = time_b - 1;
+        }
+
+        // 직전과 지금이 같으면 x
+        if(pos1[now_a] == pos2[now_b]){
+            if(pos1[now_a - 1] != pos2[now_b - 1]){
+                result++;
+            }
+        }
+
+        now_time++;
+    }
+
+    cout << result;
+
+
+
+    return 0;
+}
--- readme.md
+++ readme.md
@@ -7,7 +7,7 @@
 </p>
 
 <p align="center">
-  <img src="https://img.shields.io/badge/Total_Problems-166-4F8BFB?style=for-the-badge"/>
+  <img src="https://img.shields.io/badge/Total_Problems-167-4F8BFB?style=for-the-badge"/>
   <img src="https://img.shields.io/badge/Platforms-6-22C55E?style=for-the-badge"/>
   <img src="https://img.shields.io/badge/Roadmap-16%2F37-F59E0B?style=for-the-badge"/>
 </p>
@@ -20,11 +20,11 @@
 | --- | ---: |
 | BaekJoon | 84 |
 | SW Expert Academy | 17 |
-| Codetree | 28 |
+| Codetree | 29 |
 | Algospot | 17 |
 | Programmers | 19 |
 | Softeer | 1 |
-| **합계** | **166** |
+| **합계** | **167** |
 
 ### 카테고리 분포 (Codetree)
 
@@ -47,14 +47,14 @@ config:
 ---
 pie showData
   title Codetree 카테고리별 문제 수
-  "Trail 2" : 28
+  "Trail 2" : 29
 ```
 
 ### 🛤️ Codetree Trail 진도
 
 | Trail | 진도 | 카운트 |
 | --- | --- | --- |
-| **Trail 2** | _전체 문제 수 미설정_ | **28** solved |
+| **Trail 2** | _전체 문제 수 미설정_ | **29** solved |
 
 ### 🎯 정복한 난관 (Codetree 도전 기록 Top 5)
 
@@ -439,6 +439,7 @@ xychart-beta
 | Trail 2 | 왔다 갔던 구역 2 |
 | Trail 2 | 요일 맞추기 |
 | Trail 2 | 잔해물을 덮기 위한 사각형의 최소 넓이 |
+| Trail 2 | 좌우로 움직이는 로봇 |
 | Trail 2 | 진수 to 진수 |
 | Trail 2 | 최대로 겹치는 구간 |
 | Trail 2 | 최대로 겹치는 지점 |
