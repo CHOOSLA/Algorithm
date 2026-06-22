671c59f975e687596447c2212d2bec509c655260
ChooChangwoo <ccw.oopsla@gmail.com>
2026-06-23 02:43:43 +0900

    [Passed] Carry 피하기 2 (2ms, 0MB)

--- Codetree/trail2/Carry 피하기 2/escaping-carry-2.cpp
+++ Codetree/trail2/Carry 피하기 2/escaping-carry-2.cpp
@@ -37,7 +37,7 @@ int main() {
 
 
     // Please write your code here.
-    int result = 0;
+    int result = -1;
     for(int i=0; i < n; ++i){
         for(int j=i+1; j < n; ++j){
             for(int k=j+1; k < n; ++k){
