1c7c6d21d1d317f7005dd3cdc96c76ea84f5acce
ChooChangwoo <ccw.oopsla@gmail.com>
2026-06-22 22:49:52 +0900

    [Wrong Answer] 체크판위에서 2 (2ms, 0MB)

--- Codetree/trail2/체크판위에서 2/on-the-checkboard-2.cpp
+++ Codetree/trail2/체크판위에서 2/on-the-checkboard-2.cpp
@@ -15,13 +15,26 @@ int main() {
 
     // Please write your code here.
     int max_cnt = 0;
-    for(int i=0; i < R; ++i){
-        for(int j=0; j < C - 1; ++j){
-            for(int k = i+1; k < R; ++k){
-                for(int l=0; l < C - 1; ++l){
-                    int check1 = grid[i][j] != grid[i][j+1] ? 1 : 0;
-                    int check2 = grid[k][l] != grid[k][l+1] ? 1 : 0;
-                    max_cnt = max(max_cnt, check1+check2);
+    int now_color = grid[0][0]; // W
+    for(int i=1; i < R; ++i){
+        for(int j=1; j < C; ++j){
+            if(now_color != grid[i][j]){
+                now_color = grid[i][j]; // B
+                for(int k=i+1; k < R; ++k){
+                    for(int l=j+1; l < C; ++l){
+                        // W 검사 
+                        if(now_color != grid[k][l]){
+                            //W 면
+                            now_color = grid[k][l];
+                            
+                            // 마지막 검사
+                            if(now_color != grid[R-1][C-1]){
+                                max_cnt++;
+                                cout<< "Test" << endl;
+                            }
+                        }
+
+                    }
                 }
             }
         }
