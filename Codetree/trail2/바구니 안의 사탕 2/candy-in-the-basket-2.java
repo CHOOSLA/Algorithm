5fa8d26edda92d7bcd30d654019361eee0ae1945
ChooChangwoo <ccw.oopsla@gmail.com>
2026-06-23 23:23:20 +0900

    [Wrong Answer] 바구니 안의 사탕 2 (137ms, 10MB)

---
+++ Codetree/trail2/바구니 안의 사탕 2/candy-in-the-basket-2.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+public class Main {
+    public static int[] arr = new int[101];
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+        int[] candies = new int[n];
+        int[] positions = new int[n];
+        for (int i = 0; i < n; i++) {
+            candies[i] = sc.nextInt();
+            positions[i] = sc.nextInt();
+        }
+        // Please write your code here.
+
+        for(int i=0; i < n; ++i){
+            arr[positions[i]] += candies[i];
+        }
+
+        int result = 0;
+        for(int i=0; i <= 101 - 2 * k - 1; ++i){
+            int pre_sum = 0;
+            for(int j=i; j < i + 2 * k + 1; ++j){
+                pre_sum += arr[j];
+            }
+
+            result = Math.max(result, pre_sum);
+        }
+
+        System.out.println(result);
+    }
+}
