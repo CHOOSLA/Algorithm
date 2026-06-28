ae5b141e25ba5009a4f9fd6c882d815c54b25f50
ChooChangwoo <ccw.oopsla@gmail.com>
2026-06-29 02:00:59 +0900

    [Passed] 빙산의 일각 2 (151ms, 11MB)

--- Codetree/trail2/빙산의 일각 2/the-tip-of-the-iceberg-2.java
+++ Codetree/trail2/빙산의 일각 2/the-tip-of-the-iceberg-2.java
@@ -15,7 +15,7 @@ public class Main {
             int count = 0;
 
             int is_ice = -1;
-            for(int i=1; i < n; ++i){
+            for(int i=0; i < n; ++i){
                 if(h[i] <= height){
                     if(is_ice == 1){
                         is_ice = 0;
