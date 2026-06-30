4f9487827941954612980373d40df00ddb756f31
ChooChangwoo <ccw.oopsla@gmail.com>
2026-06-30 22:11:27 +0900

    [Wrong Answer] 이상한 폭탄 3 (130ms, 10MB)

--- Codetree/trail2/이상한 폭탄 3/strange-bomb-3.java
+++ Codetree/trail2/이상한 폭탄 3/strange-bomb-3.java
@@ -11,35 +11,39 @@ public class Main {
         // Please write your code here.
         int[] arr = bombs.clone();
         Arrays.sort(arr);
+
         
         // 이게 폭탄의 최소범위값
         int bomb_num = 0;
         int max_count = 0;
         // 폭탄 번호를 정함
         for(int i=arr[0]; i <= arr[n-1]; ++i){
-            int[] arr2 = new int[n];
-
-            // 터지게함
-            for(int j=0; j < n - k; ++j){
+            int[] check_exp = new int[n];
+            for(int j=0; j < n; ++j){
                 if(bombs[j] == i){
-                    arr2[j]++;
-                    for(int q = j; q < j + k; ++q){
-                        if(bombs[q] == i) arr2[q]++;
+                    for(int q=j + 1; q < n; ++q){
+                        if(bombs[q] == i && q - j < k){
+                            check_exp[j] = 1;
+                            check_exp[q] = 1;
+                        }
                     }
                 }
             }
-
+            
             int count = 0;
             for(int j=0; j < n; ++j){
-                if(arr2[j] >= 1) count++;
+                if(check_exp[j] == 1) count++;
             }
 
-            if(max_count < count){
-                max_count = count;
-                bomb_num = i;
-            }else if(max_count == count){
-                bomb_num = Math.max(bomb_num, i);
+            if(count > 0){
+                if(max_count < count){
+                    max_count = count;
+                    bomb_num = i;
+                }else if(max_count == count){
+                    bomb_num = Math.max(bomb_num, i);
+                }
             }
+            
         }
 
         System.out.println(bomb_num);
