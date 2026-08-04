fc77898678cd4d2fae483513c992ea125ab9bb72
ChooChangwoo <ccw.oopsla@gmail.com>
2026-08-04 14:42:45 +0900

    [Passed] A, B, C, D 찾기 (136ms, 11MB)

---
+++ Codetree/trail2/A, B, C, D 찾기/find-a-b-c-d.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] arr = new int[15];
+        for (int i = 0; i < 15; i++) {
+            arr[i] = sc.nextInt();
+        }
+        // Please write your code here.
+        Arrays.sort(arr);
+
+        int a = arr[0];
+        int b = arr[1];
+        int c = arr[2];
+        int d = arr[14] - (a+b+c);
+
+        System.out.printf("%d %d %d %d",a,b,c,d);
+    }
+}
